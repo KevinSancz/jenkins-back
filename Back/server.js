7df6aff371dd83835427f15daf34ea1442c9239b
KevinSancz <kevin.sanchez@ebiw.mx>
2025-01-28 14:33:43 -0600

    cambios

--- Back/server.js
+++ Back/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 // Ruta para consumir PokéAPI
 app.get('/pokemon/:name', async (req, res) => {
