e3519102d00934fd76e2a7a869290ef0b71c17b9
KevinSancz <kevin.sanchez@ebiw.mx>
2025-01-29 12:43:59 -0600

    cambios

--- Back/server.js
+++ Back/server.js
@@ -4,16 +4,19 @@ const axios = require('axios');
 const app = express();
 const PORT = 3000;
 
+// Ruta de health check para Kubernetes
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
 // Ruta para consumir PokéAPI
 app.get('/pokemon/:name', async (req, res) => {
-    const { name } = req.params; // Obtiene el nombre del Pokémon desde la URL
+    const { name } = req.params;
 
     try {
-        // Realiza una solicitud a la API de PokéAPI
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
         const pokemonData = response.data;
 
-        // Devuelve solo los datos relevantes al frontend
         res.json({
             name: pokemonData.name,
             height: pokemonData.height,
@@ -27,10 +30,6 @@ app.get('/pokemon/:name', async (req, res) => {
 });
 
 // Inicia el servidor
-app.listen(PORT, '0.0.0.0',() => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Backend corriendo en http://0.0.0.0:${PORT}`);
-});
-
-app.get('/healt',(req, res) => {
-    res.status(200).json({status:'ok'})
 });
